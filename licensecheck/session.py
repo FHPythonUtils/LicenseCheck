fe4404f38c55448b82fc8cfdf4ac1c0b15b1db2b
atom-andrew <andrew@atom-computing.com>
2026-06-18 11:57:36 -0700

    Allow waiting for sqlite locks in request cache

--- licensecheck/session.py
+++ licensecheck/session.py
@@ -4,4 +4,7 @@ from platformdirs import PlatformDirs
 dirs = PlatformDirs("licensecheck", "fredhappyface")
 
 
-session = requests_cache.CachedSession(dirs.user_cache_dir)
+session = requests_cache.CachedSession(
+	dirs.user_cache_dir,
+	busy_timeout=5000,
+)
